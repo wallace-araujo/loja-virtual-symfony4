aa1d126c19eb69a0e46fabe34acfc9670b19beb0
Wallace Araujo <wallace.araujo@wedigitek.com>
2019-03-05 17:17:06 -0300

    Carrinho de compras e  view do carrinho de compras... metodo add no carrinho remover do carrinho e calculo do total do carrinho

--- src/Controller/CarrinhoComprasController.php
+++ src/Controller/CarrinhoComprasController.php
@@ -2,18 +2,120 @@
 
 namespace App\Controller;
 
+use App\Entity\TbProdutos;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CarrinhoComprasController extends AbstractController
 {
+
+    public $items = [];
+
+
+    public function __construct(SessionInterface $session)
+    {
+        if($session->has('Carrinho')){
+            $Carrinho = $session->get('Carrinho', []);
+            $this->items = $Carrinho;
+            //dump($Carrinho );
+        }
+    }
+
     /**
      * @Route("/carrinho/compras", name="carrinho_compras")
+     * @Template("carrinho_compras/index.html.twig")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
-        return $this->render('carrinho_compras/index.html.twig', [
-            'controller_name' => 'CarrinhoComprasController',
-        ]);
+        $Produtos = $session->get('Carrinho', []);
+        dump($Produtos);
+        dump($this->totalCarrinho());
+        dump($this->totalItems());
+
+        return [
+            'Produtos' => $Produtos,
+            'Total' => $this->totalCarrinho(),
+        ];
     }
+
+    /**
+    * Comentario: add produto no carrinho e guardo  na sessao  futuro redis. 
+    * @Route("/add/carrinho/{id}", name="add_carrinho")
+    */
+    public function addCarrinho(TbProdutos $Produto,SessionInterface $session)
+    {
+        if(isset($this->items[$Produto->getId()])){
+            // existe o produto no carrinho add incremento na qtd
+            $this->items[$Produto->getId()] = [
+                'Item' =>$Produto,
+                'qtd' => $this->items[$Produto->getId()]['qtd'] + 1,
+            ];
+        }else{
+            // nao existe add o produto
+            $this->items[$Produto->getId()] = [
+                'Item' =>$Produto,
+                'qtd' => 1
+            ];
+        }
+        $session->set('Carrinho', $this->items);
+        //dump($this->items);
+        // exit;
+
+        //$this->addFlash('success', "Fonte de Dados '".$request->request->get('datasetname')."' foi Criada com Sucesso! ");
+        return $this->redirectToRoute('carrinho_compras');
+    }
+
+
+    /**
+    * Comentario: remover produto do carrinho diminuindo a qtd
+    * @Route("/remove/Qtdcarrinho/{id}", name="removeQtd_carrinho")
+    */
+    public function removeQtdCarrinho(TbProdutos $Produto,SessionInterface $session)
+    {
+        if(isset($this->items[$Produto->getId()]) && $this->items[$Produto->getId()]['qtd'] > 1 ){
+            $this->items[$Produto->getId()] = [
+                'Item' =>$Produto,
+                'qtd' => $this->items[$Produto->getId()]['qtd'] - 1,
+            ];
+        }elseif(isset($this->items[$Produto->getId()])){
+            unset($this->items[$Produto->getId()]);
+        }
+        $session->set('Carrinho', $this->items);
+
+        return $this->redirectToRoute('carrinho_compras');
+    }
+
+        /**
+    * Comentario: remover produto do carrinho
+    * @Route("/remove/carrinho/{id}", name="remove_carrinho")
+    */
+    public function removeCarrinho(TbProdutos $Produto,SessionInterface $session)
+    {
+        if(isset($this->items[$Produto->getId()])){
+            unset($this->items[$Produto->getId()]);
+        }
+        $session->set('Carrinho', $this->items);
+
+        return $this->redirectToRoute('carrinho_compras');
+    }
+
+
+    public function totalCarrinho()
+    {
+        $total = 0;
+        foreach($this->items as $item){
+            //dump($item['Item']->getpreco());
+            $subtotal = $item['Item']->getpreco() * $item['qtd'];
+            $total += $subtotal;
+        }
+        return $total;
+    }
+
+    public function totalItems()
+    {
+        return count($this->items);
+    }
+
 }
